4828304459f9aed7747bef583ce321532e2a26db
Your Name <carlaguaman06@gmail.com>
2026-03-27 13:23:17 -0500

    Ejercicio 1 Prueba (El código esta bien)

---
+++ ejercicio_prueba.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1;
+    cout<<"Ingrese el primer numero: ";
+    cin>>num1;
+    int num2;
+    cout<<"Ingrese el segundo numero: ";
+    cin>>num2;  
+    int suma,resta,multi;
+    suma=num1+num2;
+    resta=num1-num2;
+    multi=num1*num2;
+cout<<"La suma es: "<<suma<<endl;
+cout<<"La resta es: "<<resta<<endl;     
+cout<<"La multiplicacion es: "<<multi<<endl;
+    return 0;
+    
+}
